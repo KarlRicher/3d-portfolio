e812776e18bb1fe9ff99cf109a7a09aae5c2d61a
Karl Richer <karl.richer@outlook.com>
2023-08-02 11:25:51 -0400

    Stars + emailjs in Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { slideIn } from "../utils/motions"
 
+// v0zk7iN5-7uLE-FV0
+// template_w185n6s
+// service_a2ov45r
+
 const Contact = () => {
   const formRef = useRef()
   const [form, setForm] = useState({
@@ -16,9 +20,49 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs
+      .send(
+        "service_ekbcf5y",
+        "template_w185n6s",
+        {
+          from_name: form.name,
+          to_name: "Karl",
+          from_email: form.email,
+          to_email: "karl.richer@outlook.com",
+          message: form.message,
+        },
+        "v0zk7iN5-7uLE-FV0"
+      )
+      .then(
+        () => {
+          setLoading(false)
+          alert("Thank you. I will get back to you as soon as possible.")
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          })
+        },
+        (error) => {
+          setLoading(false)
+
+          console.log(error)
+
+          alert("Something went wrong.")
+        }
+      )
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
